247513d3cff63839aa6132a3b1ebffa31345fcf8
keelw <wjordankeel@gmail.com>
2024-03-14 13:58:35 -0600

    fixing mongodb connection and troubleshooting

--- controllers/budget.js
+++ controllers/budget.js
@@ -1,5 +1,5 @@
 mongodb = require('../db/connect');
-const { ObjectId } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async(req, res) => {
     //#swagger.tags=['Budget']
@@ -12,7 +12,7 @@ const getAll = async(req, res) => {
 
 const getSingle = async(req, res) => {
     //#swagger.tags=['Budget']
-    const budgetId = ObjectId(req.params.id);
+    const budgetId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('cse341-budget').findOne({ _id: budgetId });
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
@@ -34,7 +34,7 @@ const create = async(req, res) => {
         notes: req.body.notes
     };
 
-    const response = await mongodb.getDatabase().db().collection('budget').insertOne(budget);
+    const response = await mongodb.getDatabase().db().collection('cse341-budget').insertOne(budget);
     if (response.acknowledged) {
         res.status(204).send();
     } else {
